87111cb3a941a848a8777ff892cafc2551236d33
Jonathan Duya <duyaj@uci.edu>
2023-07-15 08:45:12 -0700

    Initial Commit

---
+++ ZotConnect/ZotConnectApp.swift
@@ -0,0 +1,17 @@
+//
+//  ZotConnectApp.swift
+//  ZotConnect
+//
+//  Created by Jonathan Duya on 7/15/23.
+//
+
+import SwiftUI
+
+@main
+struct ZotConnectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
