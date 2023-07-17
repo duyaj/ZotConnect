bb845408a179ef9dc8193f46c36cca35ebb698da
csohare <csohare@uci.edu>
2023-07-17 10:24:22 -0700

    firebase on main

--- ZotConnect/ZotConnectApp.swift
+++ ZotConnect/ZotConnectApp.swift
@@ -6,9 +6,14 @@
 //
 
 import SwiftUI
+import Firebase
 
 @main
 struct ZotConnectApp: App {
+    
+    init() {
+        FirebaseApp.configure()
+    }
     var body: some Scene {
         WindowGroup {
             SplashScreenView()
