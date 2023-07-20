90e98854b2bcfc87b3ce3d2bd5bc87434f8beb1e
Jonathan Duya <duyaj@uci.edu>
2023-07-20 08:49:00 -0700

    Filter model revised

--- ZotConnect/FilterFeedView.swift
+++ ZotConnect/FilterFeedView.swift
@@ -15,51 +15,57 @@ struct FilterFeedView: View {
     
     var body: some View {
         List {
-            HStack {
-                Button {
-                    if tintColor1 == .purple {
-                        tintColor1 = nil
-                    } else {
-                        tintColor1 = .purple
+                HStack {
+                    Button {
+                        if tintColor1 == .purple {
+                            tintColor1 = nil
+                        } else {
+                            tintColor1 = .purple
+                        }
+                        
+                    } label: {
+                        Label("New", systemImage: "seal.fill")
                     }
+                    .buttonStyle(.bordered)
+                    .foregroundColor(.purple)
+                    .tint(tintColor1)
                     
-                } label: {
-                    Label("New", systemImage: "seal.fill")
-                }
-                .buttonStyle(.bordered)
-                .foregroundColor(.purple)
-                .tint(tintColor1)
-        
-                Button {
-                    if tintColor2 == .red {
-                        tintColor2 = nil
-                    } else {
-                        tintColor2 = .red
+                    Button {
+                        if tintColor2 == .red {
+                            tintColor2 = nil
+                        } else {
+                            tintColor2 = .red
+                        }
+                        
+                    } label: {
+                        Label("Hot", systemImage: "flame.fill")
                     }
+                    .buttonStyle(.bordered)
+                    .foregroundColor(.red)
+                    .tint(tintColor2)
                     
-                } label: {
-                    Label("Hot", systemImage: "flame.fill")
-                }
-                .buttonStyle(.bordered)
-                .foregroundColor(.red)
-                .tint(tintColor2)
-                
-                Button {
-                    if tintColor3 == .blue {
-                        tintColor3 = nil
-                    } else {
-                        tintColor3 = .blue
+                    Button {
+                        if tintColor3 == .blue {
+                            tintColor3 = nil
+                        } else {
+                            tintColor3 = .blue
+                        }
+                        
+                    } label: {
+                        Label("Top", systemImage: "arrow.up.circle.fill")
                     }
+                    .buttonStyle(.bordered)
+                    .foregroundColor(.blue)
+                    .tint(tintColor3)
                     
-                } label: {
-                    Label("Top", systemImage: "arrow.up.circle.fill")
-                }
-                .buttonStyle(.bordered)
-                .foregroundColor(.blue)
-                .tint(tintColor3)
-                }
+                }.frame(minHeight: minRowHeight, alignment: .center)
+                .padding(.horizontal)
             
-        }.frame(minHeight: minRowHeight * 2).scrollDisabled(true).scrollContentBackground(.hidden)
+                
+        }.listStyle(PlainListStyle())
+            .frame(minHeight: minRowHeight * 1.3)
+            .scrollDisabled(true)
+            .scrollContentBackground(.hidden)
         }
     }
     
