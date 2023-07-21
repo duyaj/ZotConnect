7e4d6e4936faedafa51e98d83df243409918b5a2
csohare <csohare@uci.edu>
2023-07-20 22:11:35 -0700

    orgPostService

--- ZotConnect/GroupProfile/Services/OrgPostService.swift
+++ ZotConnect/GroupProfile/Services/OrgPostService.swift
@@ -16,9 +16,7 @@ struct orgPostService {
         let db = Firestore.firestore()
         db.collection("/orgs/\(org.id)/Announcement").getDocuments { snapshot, err in
             guard let docs = snapshot?.documents else {return}
-            let announcements = docs.map { snapshot in
-                
-            }
+            let announcements = docs.compactMap({try? $0.data(as: Post.self)})
             completion(announcements)
         }
         
