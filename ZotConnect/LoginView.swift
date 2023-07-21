d38ff279085b757298714daf71f5aa8e22a6a8fc
Jonathan Duya <duyaj@uci.edu>
2023-07-20 20:39:01 -0700

    Test push to fix damaged files

--- ZotConnect/LoginView.swift
+++ ZotConnect/LoginView.swift
@@ -7,37 +7,27 @@
 
 import SwiftUI
 
-import SwiftUI
-
 struct LoginView: View {
     var body: some View {
-        VStack {
-            Spacer()
-            
-            Button(action: {
-            }) {
-                HStack {
-                    Image("ZotSplash")
-                        .resizable()
-                        .aspectRatio(contentMode: .fit)
-                        .frame(width: 24, height: 24)
-                        .foregroundColor(.white)
-                    
-                    Text("Sign in with UCI")
-                        .foregroundColor(.white)
-                        .font(.headline)
-                        .padding()
-                }
-                .background(Color.blue)
-                .cornerRadius(8)
-            }
-            
-            Spacer()
-        }
-        .padding()
-    }
-}
-
+           VStack {
+               Spacer()
+               
+               Button(action: {
+                   //
+               }) {
+                   Text("Sign in with UCI")
+                       .foregroundColor(.white)
+                       .font(.headline)
+                       .padding()
+                       .background(Color.blue)
+                       .cornerRadius(8)
+               }
+               
+               Spacer()
+           }
+           .padding()
+       }
+   }
 
 struct LoginView_Previews: PreviewProvider {
     static var previews: some View {
