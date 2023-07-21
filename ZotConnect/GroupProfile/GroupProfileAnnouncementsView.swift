702f6a5864d3c2958efc6bd012fdde20a3c0bffd
Jonathan Duya <duyaj@uci.edu>
2023-07-20 23:31:26 -0700

    Files

--- ZotConnect/GroupProfile/GroupProfileAnnouncementsView.swift
+++ ZotConnect/GroupProfile/GroupProfileAnnouncementsView.swift
@@ -1,21 +1,20 @@
 //
-//  LoginView.swift
+//  GroupProfileAnnouncementsView.swift
 //  ZotConnect
 //
 //  Created by Jonathan Duya on 7/20/23.
 //
 
 import SwiftUI
-import Firebase
 
-struct LoginView: View {
+struct GroupProfileAnnouncementsView: View {
     var body: some View {
         Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
     }
 }
 
-struct LoginView_Previews: PreviewProvider {
+struct GroupProfileAnnouncementsView_Previews: PreviewProvider {
     static var previews: some View {
-        LoginView()
+        GroupProfileAnnouncementsView()
     }
 }
