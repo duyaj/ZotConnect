77bdc739435d02c7cc6cdc0486375689726a8cce
Jonathan Duya <duyaj@uci.edu>
2023-07-20 20:33:22 -0700

    Error handling push

--- ZotConnect/LoginView.swift
+++ ZotConnect/LoginView.swift
@@ -7,11 +7,37 @@
 
 import SwiftUI
 
+import SwiftUI
+
 struct LoginView: View {
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        VStack {
+            Spacer()
+            
+            Button(action: {
+            }) {
+                HStack {
+                    Image("ZotSplash")
+                        .resizable()
+                        .aspectRatio(contentMode: .fit)
+                        .frame(width: 24, height: 24)
+                        .foregroundColor(.white)
+                    
+                    Text("Sign in with UCI")
+                        .foregroundColor(.white)
+                        .font(.headline)
+                        .padding()
+                }
+                .background(Color.blue)
+                .cornerRadius(8)
+            }
+            
+            Spacer()
+        }
+        .padding()
     }
 }
+
 
 struct LoginView_Previews: PreviewProvider {
     static var previews: some View {
